e54bab162e492dc9c9f544979defc9b4de3155ea
Claudius <mbembac@gmail.com>
2014-07-29 23:46:26 -0400

    site completed with preloader

---
+++ js/load.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("body").queryLoader2({
+        barColor:"#EEEEEE",
+        backgroundColor: "#6e6d73",
+        percentage: true,
+        barHeight: 1,
+        completeAnimation: "grow",
+        minimumTime: 100
+    });
+});
