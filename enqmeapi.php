5e7706af7aa0c49fe5d55cda83914cb56214a961
Claudius Mbemba <mbembac@gmail.com>
2014-10-04 14:00:40 -0400

    edited enQ api

--- enqmeapi.php
+++ enqmeapi.php
@@ -3,53 +3,32 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 
-/* API Wrapper */
+/* API  */
+global $curr_num , $prev_num;
 
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+  case 'GET':
+  if(!empty($curr_num)){
+    $order_num = $curr_num;
+  }else{
+    $order_num = "1";
+  }
+  break;
+
+  case 'POST':
+  $prev_num = $curr_num;
+  $curr_num = htmlentities($_POST['order_num']);
+  $order_num = $curr_num;
+  break;
+
+  default:
+  $order_num = "Bad request";
+  break;
+}
 // $order_num = htmlentities($_GET['order_num']);
 
-$order_num = htmlentities('74');
-
-// $url = "http://thegamesdb.net/api/GetGamesList.php?name=".$title;
-
-// $ch = curl_init($url);
-
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// $response = curl_exec($ch);
-// curl_close($ch);
-
-//   // var_dump($response);
-
-// $arr = convertXML($response);
-
-//   // var_dump($arr);
-
-//   // print(json_encode($arr));
-
-// $gameID = $arr["Game"][0]['id'];
-// // echo "id: ".$gameID;
-
-// $url = "http://thegamesdb.net/api/GetArt.php?id=".$gameID;
-
-// $ch = curl_init($url);
-
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// $response = curl_exec($ch);
-// curl_close($ch);
-
-// $arr = convertXML($response);
-
-//   // var_dump($arr);
-
-// $art = $arr['Images']["fanart"];
-
-// var_dump($art);
-// for ($i=0; $i < count($art) ; $i++) {
-//  echo $art[$i]['original'];
-// }
 
 print(json_encode($order_num));
 
