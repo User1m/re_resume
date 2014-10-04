a12da095320012cad8cf63d53df8efb0e2e64cb2
Claudius Mbemba <mbembac@gmail.com>
2014-10-04 15:30:46 -0400

    edited api

--- enqmeapi.php
+++ enqmeapi.php
@@ -20,14 +20,16 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
 
-  // //get method
+  //get method
   case 'GET':
+
   if($_SESSION['store']){
     $order_num = $_SESSION['store'];
     $_SESSION['store'] = $order_num;
   }else{
     $order_num = "empty";
   }
+
   break;
 
   //post method
@@ -47,24 +49,8 @@ switch ($method) {
   $order_num = "Bad request";
   break;
 }
-// $order_num = htmlentities($_GET['order_num']);
-
 
 print(json_encode($order_num));
 
 exit();
 ?>
-
-<?php
-
-// function convertXML($xmldata){
-
-//   $xml = simplexml_load_string($xmldata);
-//   $json = json_encode($xml);
-//   $arr = json_decode($json, TRUE);
-
-//   return $arr;
-// }
-
-?>
-
