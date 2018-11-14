78fa7e01ab9faca757741f57ab2034c8177e682e
Claudius Mbemba <mbembac@gmail.com>
2018-11-13 17:05:30 -0800

    updated deploy

--- docker/scripts/deploy.sh
+++ docker/scripts/deploy.sh
@@ -6,13 +6,14 @@ az account set -s "79127f51-3017-480c-a70e-0f3f20694d2e"
 DOCKER_IMAGE=user1m/resumeweb:prod
 RG=business
 APP_SERVICE_PLAN=BizAppServices
+APP_NAME=claudiusresume #claudiusmbemba
 
 az webapp create --resource-group $RG \
---plan $APP_SERVICE_PLAN --name claudiusmbemba --deployment-container-image-name $DOCKER_IMAGE
+--plan $APP_SERVICE_PLAN --name $APP_NAME --deployment-container-image-name $DOCKER_IMAGE
 
 ### https://docs.microsoft.com/en-us/cli/azure/webapp/config/appsettings?view=azure-cli-latest#az_webapp_config_appsettings_set
 az webapp config appsettings set --resource-group $RG \
---name claudiusmbemba \
+--name $APP_NAME \
 --settings WEBSITES_PORT=80 \
 PORT=80 \
 WEBSITE_NODE_DEFAULT_VERSION=9.4 \
@@ -22,9 +23,9 @@ MAIL_PASSWORD=L^8o!995Bo*p9!Lw \
 ## CI/CD
 ### https://docs.microsoft.com/en-us/azure/app-service/containers/app-service-linux-ci-cd
 # echo "Take the CI_CD_URL below and create a Docker Hub Webhook..."
-# az webapp deployment container config --name claudiusmbemba --resource-group $RG --enable-cd true
+az webapp deployment container config --name $APP_NAME --resource-group $RG --enable-cd true
 
-az webapp restart --resource-group $RG --name claudiusmbemba
+az webapp restart --resource-group $RG --name $APP_NAME
 
 ### SSH LOCALLY
 # https://docs.microsoft.com/en-us/azure/app-service/containers/app-service-linux-ssh-support
